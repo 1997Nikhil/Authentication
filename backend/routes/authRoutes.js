8bc887d61c6d10a23658ed96191e02579958d56c
Nikhil Dadhich <nikhildadhich58@gmail.com>
2026-08-24 13:52:43 +0530

    Adding comments

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,11 +5,11 @@ const {
   login,
 } = require("../controllers/authController.js");
 
-const router = express.Router();
+const router = express.Router(); // Create a new router instance to define routes related to authentication.
 
-router.post("/register", register);
+router.post("/register", register); // This line defines a POST route for user registration. When a POST request is made to /api/auth/register, the register function from authController.js will be called to handle the request.
 
-router.post("/login", login);
+router.post("/login", login); // This line defines a POST route for user login. When a POST request is made to /api/auth/login, the login function from authController.js will be called to handle the request.
 
 
 module.exports = router;
