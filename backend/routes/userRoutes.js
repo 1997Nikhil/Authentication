56d5b696237a9e2bde5553231b724a3f4d10ff72
Nikhil Dadhich <nikhildadhich58@gmail.com>
2026-08-24 13:56:20 +0530

    Adding comments

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ const authMiddleware = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, (req, res) => {
+router.get("/profile", authMiddleware, (req, res) => {  // This line defines a GET route for fetching the user's profile. The authMiddleware is applied to this route, which means that the user must provide a valid JWT token in the Authorization header to access this route. If the token is valid, the request will proceed to the callback function; otherwise, an error response will be sent.
   res.json({
     message: "Protected profile data",
     user: req.user,
